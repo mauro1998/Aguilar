c1e92c8ed645f46b996f4b5a7a8c7055bdf96ee7
Mauro Aguilar <mauro_aguilar98@hotmail.com>
2017-03-22 23:36:04 -0500

    Getting started with the skyline structure

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const index = require('./routes/index');
+const users = require('./routes/users');
 const app = express();
-const port = 3000;
 
-app.use(express.static('public'));
+// view engine setup
+hbs.registerPartials(path.join(__dirname, 'templates/partials'));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'hbs');
 
-app.get('*', (req, res) => {
-  const index = path.join(__dirname, 'index.html');
-  res.sendFile(index);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.listen(port, () => {
-  console.log('App running at 0.0.0.0:' + port);
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('layout', { error: true });
 });
+
+module.exports = app;
--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,21 @@
 // Modules
 const path = require('path');
 const gulp = require('gulp');
-const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 const autoprefix = require('gulp-autoprefixer');
 
 // SASS directories
-const dir = 'style_guide/css';
+const dir = path.join(__dirname, 'sass');
 const src = path.join(dir, 'main.scss');
 const dest = 'public/assets/styles';
 
 // Tasks
-gulp.task('make-css', function() {
-  return gulp.src(src)
+gulp.task('make-css', () => gulp.src(src)
   .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
   .pipe(autoprefix({ browsers: ['last 2 versions'], cascade: false }))
   .pipe(gulp.dest(dir))
-  .pipe(gulp.dest(dest));
-});
+  .pipe(gulp.dest(dest)));
 
-gulp.task('sass:watch', function() {
-  return gulp.watch(path.join(dir, '**/*.scss'), ['make-css']);
-});
+gulp.task('sass:watch', () => gulp.watch(path.join(dir, '**/*.scss'), ['make-css']));
 
-gulp.task('copy-vendor', function() {
-  return gulp.src([
-    'node_modules/requirejs/require.js',
-    'node_modules/requirejs-text/text.js',
-    'node_modules/underscore/underscore.js',
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/backbone/backbone.js'
-  ]).pipe(uglify())
-  .pipe(gulp.dest('public/vendor'));
-});
-
-gulp.task('default', ['make-css', 'copy-vendor']);
+gulp.task('default', ['make-css']);
